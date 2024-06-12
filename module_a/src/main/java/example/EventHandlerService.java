775e3ae71933bf663285e4a43bb4a5a8a01fb856
Dominik Tworek <dtworek94@gmail.com>
2024-06-12 05:01:15 +0100

    fiexed missing modules dependencies

--- module_a/src/main/java/example/EventHandlerService.java
+++ module_a/src/main/java/example/EventHandlerService.java
@@ -1,11 +1,13 @@
 package example;
 
 import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
 
+@Component
 public class EventHandlerService {
     
     @EventListener
     public void handleCustomEvent(CustomEvent event) {
-        System.out.println("USER CREATED");
+        System.out.println("Message Created!");
     } 
 }
