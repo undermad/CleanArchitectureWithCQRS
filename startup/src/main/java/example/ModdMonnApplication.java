c8898002f9e359a08dfb729cd0b6f81bab1c889b
Dominik Tworek <dtworek94@gmail.com>
2024-06-22 16:21:22 +0100

    Setted up mysql configuration and entity scanning.

--- startup/src/main/java/example/ModdMonnApplication.java
+++ startup/src/main/java/example/ModdMonnApplication.java
@@ -1,11 +1,17 @@
 package example;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 
+@Log4j2
 @SpringBootApplication
+//@EnableJpaRepositories
+@EntityScan(basePackages = {"ectimel"})
 public class ModdMonnApplication {
 
     public static void main(String[] args) {
